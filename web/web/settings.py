43b2000a1176283bab8b1632b95ed8cfd6c1adf9
mario12508 <mario12508@mail.ru>
2025-09-09 19:41:39 +0300

    supabase pgsql db

--- web/web/settings.py
+++ web/web/settings.py
@@ -90,18 +90,12 @@ TEMPLATES = [
 WSGI_APPLICATION = "web.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-# DATABASES = {
-#     "default": dj_database_url.config(
-#         default=os.getenv("DATABASE_URL"),
-#         conn_max_age=600,
-#         ssl_require=True
-#     )
-# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
